02080051f062d4e770a1445c64e96e8ab8ef0328
Ricky Elrod <ricky@elrod.me>
2016-07-25 18:53:18 -0400

    more hax
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- common.php
+++ common.php
@@ -1,11 +1,12 @@
 <?php
-$pgomap_root = '/srv/PokemonGo-map';
+$pgomap_root = '/srv/PokemonGo-Map';
 date_default_timezone_set('America/New_York');
 
 $p = json_decode(file_get_contents($pgomap_root.'/static/locales/pokemon.en.json'), true);
 
 class DB extends SQLite3 {
   function __construct() {
+    global $pgomap_root;
     $this->open($pgomap_root.'/pogom.db', SQLITE3_OPEN_READONLY);
   }
 }
